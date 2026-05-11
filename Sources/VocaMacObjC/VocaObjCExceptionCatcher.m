679204370c539520336da4bca1ac1b032c9937bf
Jatin K Malik <jmalik@atlassian.com>
2026-05-11 14:22:04 -0700

    fix: harden audio engine tap lifecycle

---
+++ Sources/VocaMacObjC/VocaObjCExceptionCatcher.m
@@ -0,0 +1,25 @@
+// VocaObjCExceptionCatcher.m
+// VocaMac
+
+#import "VocaMacObjC.h"
+
+@implementation VocaObjCExceptionCatcher
+
++ (NSError * _Nullable)catchException:(NS_NOESCAPE void (^)(void))block {
+    @try {
+        block();
+        return nil;
+    } @catch (NSException *exception) {
+        NSString *name = exception.name ?: @"NSException";
+        NSString *reason = exception.reason ?: @"Objective-C exception raised";
+
+        return [NSError errorWithDomain:@"com.vocamac.objc-exception"
+                                   code:1
+                               userInfo:@{
+                                   NSLocalizedDescriptionKey: reason,
+                                   @"NSExceptionName": name
+                               }];
+    }
+}
+
+@end
---
+++ Tests/VocaMacTests/ObjCExceptionCatcherTests.swift
@@ -0,0 +1,32 @@
+// ObjCExceptionCatcherTests.swift
+// VocaMac
+
+import Foundation
+import VocaMacObjC
+import XCTest
+
+final class ObjCExceptionCatcherTests: XCTestCase {
+
+    func testCatchExceptionReturnsNilWhenBlockDoesNotRaise() {
+        let error = VocaObjCExceptionCatcher.catchException {
+            _ = "safe block"
+        }
+
+        XCTAssertNil(error)
+    }
+
+    func testCatchExceptionConvertsNSExceptionToNSError() throws {
+        let error = VocaObjCExceptionCatcher.catchException {
+            NSException(
+                name: NSExceptionName("VocaMacTestException"),
+                reason: "simulated Objective-C exception",
+                userInfo: nil
+            ).raise()
+        }
+
+        let unwrappedError = try XCTUnwrap(error as NSError?)
+        XCTAssertEqual(unwrappedError.domain, "com.vocamac.objc-exception")
+        XCTAssertEqual(unwrappedError.localizedDescription, "simulated Objective-C exception")
+        XCTAssertEqual(unwrappedError.userInfo["NSExceptionName"] as? String, "VocaMacTestException")
+    }
+}
